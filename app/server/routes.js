43d0731c2f35ee2b234ae9d530d1fdd9b2f6c7c5
Kari Eronen <kari.eronen71@gmail.com>
2018-01-18 10:42:06 +0200

    2nd phase, final

---
+++ app/server/routes.js
@@ -0,0 +1,9 @@
+const routes = [
+	'/',
+	'/pokemon',
+	'/pokemon/ability/:ability'
+]
+
+export default routes;
+
+
---
+++ app/services/getPokemon.js
@@ -0,0 +1,12 @@
+import request from 'axios';
+
+const getPokemon = {
+	withAbility: (ability) => {
+		const baseUrl = 'http://pokeapi.co/api/v2/ability';
+		return request.get(`${baseUrl}/${ability}`)
+	}
+};
+
+export default getPokemon;
+
+
